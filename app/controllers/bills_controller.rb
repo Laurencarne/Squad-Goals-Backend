ef463ef22e8a6098e7f75aae2079685be68b8a02
Lauren Carne <oreocookie@hotmail.co.uk>
2019-07-26 16:59:43 +0100

    Working on bill split

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -2,13 +2,16 @@ class BillsController < ApplicationController
   before_action :validate_flat
 
   def create
+    debugger
     bill = Bill.create(bill_params)
+
     render json: bill
   end
 
   private
   def bill_params
-    params.require(:bill).permit(:name, :totle, :desc, :flatmate_id)
+    params[:bill][:flatmate_ids] ||= []
+    params.require(:bill).permit(:name, :title, :desc, :flatmate_id, :total, flatmate_ids: [:bill_id, :flatmate_id, :total_owed, :paid])
   end
 
 
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,15 @@
 class Bill < ApplicationRecord
   has_many :bill_splits
   has_many :flatmates, through: :bill_splits
+
+  def flatmate_ids=(flatmate_ids)
+    debugger
+    flatmate_ids.map do |flatmate|
+      self.bill_splits.build(flatmate).save
+    end
+    debugger
+  end
+
+
+
 end
